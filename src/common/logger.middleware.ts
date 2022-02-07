6a63e789089fc7c6dd04fd5e62bffdf968cc560c
文竹 <hengxinsky@dingtalk.com>
2022-02-07 23:29:53 +0800

    错误上报

--- src/common/logger.middleware.ts
+++ src/common/logger.middleware.ts
@@ -6,6 +6,10 @@ export default function LoggerGlobal(req: Request, _, next: () => void) {
   const { method, path } = req;
   if (method !== 'OPTIONS') {
     Logger.log(`[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${method} ${path}`);
+
+    if (path.includes('/api/log')) {
+      Logger.error(req.query.error);
+    }
   }
   next();
 }
